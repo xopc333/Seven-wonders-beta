888725555fb6c496535df594ce068ba96d13b7d3
xopc333 <xopc333@gmail.com>
2023-08-14 16:30:05 +0700

    add pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   css: ["~/assets/styles/main.sass"],
+  modules: [ '@pinia/nuxt' ],
   devtools: {enabled: true},
   // experimental: {
   //   payloadExtraction: false
